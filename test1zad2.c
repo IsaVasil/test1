b5fd58f42f58300868953ea91b019834a43ef3d1
IsaVasil <105173734+IsaVasil@users.noreply.github.com>
2022-07-18 11:37:16 +0300

    Add files via upload

--- test1zad2.c
+++ test1zad2.c
@@ -5,32 +5,32 @@
 unsigned zerosCount(uint64_t mask1, uint32_t mask2, uint16_t mask3, uint8_t mask4){
     int cnt=0;
    
-for (int i=0;i<sizeof(mask1);i++){
+
     for(int a=0;a<sizeof(mask1);a++){
-    if(!(mask1[i]&(1ull << a))==0){
+    if(!(mask1 &(1ull << a))==0){
         cnt++;
     }
     }
-}
-   for (int i=0;i<sizeof(mask2);i++){
+
+   
     for(int a=0;a<sizeof(mask2);a++){
-    if(!(mask2[i]&(1ull << a))==0){
+    if(!(mask2 &(1ull << a))==0){
         cnt++;
     }
-    }
+    
 }
-    for (int i=0;i<sizeof(mask3);i++){
+    
     for(int a=0;a<sizeof(mask3);a++){
-    if(!(mask3[i]&(1ull << a))==0){
+    if(!(mask3 &(1ull << a))==0){
         cnt++;
     }
-    }
+    
 }
-    for (int i=0;i<sizeof(mask4);i++){
+    
     for(int a=0;a<sizeof(mask4);a++){
-    if(!(mask4[i]&(1ull << a))==0){
+    if(!(mask4&(1ull << a))==0){
         cnt++;
-    }
+    
     }
 }
     
@@ -39,7 +39,7 @@ for (int i=0;i<sizeof(mask1);i++){
     
 
 
-void flipOddBits(uint64_t* mask,, size_t bit){
+void flipOddBits(uint64_t* mask, size_t bit){
 
     int index = bit / (sizeof(*mask) * __CHAR_BIT__);
     for(int i=0;i<(sizeof(*mask) * __CHAR_BIT__);i+2){
